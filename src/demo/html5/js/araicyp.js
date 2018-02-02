70cdf9b6cfdec33fb287108a4b7031f8ff37ef8d
Sebastian A. Weiss <sebastian.weiss@nacamar.de>
2018-02-02 16:26:09 +0100

    extended logging of skip result

--- src/demo/html5/js/araicyp.js
+++ src/demo/html5/js/araicyp.js
@@ -6,43 +6,45 @@
 
 /**
  * @param {String}
- *                host - host of streaming server
+ *            host - host of streaming server
  * @param {String}
- *                path - path of requested resource
+ *            path - path of requested resource
  * @param {Function}
- *                callback - callback function for retrieving session id
+ *            callback - callback function for retrieving session id
  */
 function createSession(host, path, callback) {
-    var xmlhttp = new XMLHttpRequest();
-    var url = "http://" + host + "/" + path + "/ctrl/create-session";
-    xmlhttp.onreadystatechange = function() {
-        if ((this.readyState == 4) && (this.status == 200)) {
-            var response = JSON.parse(this.responseText);
-            callback(response.sessionId);
-        }
-    };
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
+	var xmlhttp = new XMLHttpRequest();
+	var url = "http://" + host + "/" + path + "/ctrl/create-session";
+	xmlhttp.onreadystatechange = function() {
+		if ((this.readyState == 4) && (this.status == 200)) {
+			var response = JSON.parse(this.responseText);
+			callback(response.sessionId);
+		}
+	};
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
 }
 
 /**
  * @param {String}
- *                hostVal - host of streaming server
+ *            hostVal - host of streaming server
  * @param {String}
- *                pathVal - path of requested resource
+ *            pathVal - path of requested resource
  * @param {String}
- *                sessionIdVal - session id
+ *            sessionIdVal - session id
  */
 function skip(hostVal, pathVal, sessionIdVal) {
-    var xmlhttp = new XMLHttpRequest();
-    var url = "http://" + hostVal + "/" + pathVal + "/ctrl/skip?sessionId="
-            + sessionIdVal;
-    xmlhttp.onreadystatechange = function() {
-        if ((this.readyState == 4) && (this.status == 200)) {
-            var response = JSON.parse(this.responseText);
-            console.info("number of skips left: " + response.skipsLeft);
-        }
-    };
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
+	var xmlhttp = new XMLHttpRequest();
+	var url = "http://" + hostVal + "/" + pathVal + "/ctrl/skip?sessionId="
+			+ sessionIdVal;
+	xmlhttp.onreadystatechange = function() {
+		if ((this.readyState == 4) && (this.status == 200)) {
+			var response = JSON.parse(this.responseText);
+			console.info("skip response [skipsLeft: " + response.skipsLeft
+					+ ", nextSkipReturnsToMain: "
+					+ response.nextSkipReturnsToMain + "].");
+		}
+	};
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
 }
