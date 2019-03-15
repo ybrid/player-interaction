267e82263df0231ae6b2882f4c9f24a6503ed9a8
Sebastian A. Weiss <sebastian.weiss@nacamar.de>
2019-03-15 09:10:00 +0100

    updated available demo contents

--- demo/html5/bare/ybrid/conf.ybrid.js
+++ demo/html5/bare/ybrid/conf.ybrid.js
@@ -5,10 +5,19 @@
  */
 
 const scheme = 'https';
+const host = 'stagecast.ybrid.io';
 
-/** host of streaming server */
-const host = 'democast.ybrid.io';
-const path = '/rtl.mp3';
+//### Available demos ####
+
+//### 1. Shows adaptivity capabilities by modulating outgoing bandwidth up and down between 32 and 192 kbps 
+const path = '/adaptive-demo';
+
+//### 2. Shows ad injection capabilities, including pre-stream ad injection and in-stream ad-injection 
+//const path = '/ad-injection-demo';
+
+//### 3. Since both demos above are based on Ybrid's playlist support, this third demo uses a real DABiS800 
+//       Signal ingested by a playout system located in Switzerland :-) 
+//const path = '/dabis/icecast/fixed';
 
 // whether companion ads shall pop up or not.
 const showCompanions = false;
--- demo/html5/extended/ybrid/conf.ybrid.js
+++ demo/html5/extended/ybrid/conf.ybrid.js
@@ -4,5 +4,20 @@
  * @author Sebastian A. Weiß (C) 2018 nacamar GmbH
  */
 
-// whether companion ads shall pop up or not. 
+const scheme = 'https';
+const host = 'stagecast.ybrid.io';
+
+//### Available demos ####
+
+//### 1. Shows adaptivity capabilities by modulating outgoing bandwidth up and down between 32 and 192 kbps 
+const path = '/adaptive-demo';
+
+//### 2. Shows ad injection capabilities, including pre-stream ad injection and in-stream ad-injection 
+//const path = '/ad-injection-demo';
+
+//### 3. Since both demos above are based on Ybrid's playlist support, this third demo uses a real DABiS800 
+//       Signal ingested by a playout system located in Switzerland :-) 
+//const path = '/dabis/icecast/fixed';
+
+// whether companion ads shall pop up or not.
 const showCompanions = false;
